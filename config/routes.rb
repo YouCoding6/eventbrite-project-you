a8faebb563f9128a847e39bbd0795112d0cb577a
YoucefKhalainou <youcef_khalainou@yahoo.fr>
2019-11-06 01:47:57 +0100

    index+show

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'events#index'
   resources :users
+  resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-Event.destroy_all
-Attendance.destroy_all
-User.destroy_all
+require 'faker'
 
 
-5.times do
-  u = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::ChuckNorris.fact)
-  u.email = u.first_name + '_' + u.last_name + '@mail.com'
-  u.save
+nb_user = 20  
+nb_events = 10
+nb_guests = 20
+
+
+nb_user.times do |x|
+  fname = Faker::Name.first_name
+  User.create(
+      email: fname+'@yopmail.com',
+      first_name:  fname,
+      password: 'azerty',
+      last_name: Faker::Name.last_name,
+      description: Faker::Lorem.paragraph_by_chars(number: 200, supplemental: false))
+  puts "Seeding of User nb #{x}"
 end
 
-puts 'User created !'
+#for the random startdate
+t1 = Time.parse("2019-11-01 14:40:34")
+t2 = Time.parse("2022-01-01 00:00:00")
 
-5.times do
-  Event.create(title: Faker::DcComics.title, start_date: Faker::Date.forward(days: 10), duration: [5,10,20,30].sample, description: Faker::Lorem.sentence(word_count: 15), price: Faker::Number.between(from: 1, to: 1000), location: Faker::JapaneseMedia::SwordArtOnline.location, admin_id: rand(1..5))
+nb_events.times do |x|
+  Event.create(
+    start_date: rand(t1..t2),
+    duration: rand(5..100)*5,
+    description: Faker::Lorem.paragraph_by_chars(number: 300, supplemental: false),
+    location: Faker::Address.city,
+    price: rand(1..1000),
+    title: Faker::Book.title,
+    admin_id: User.all.sample.id)
+  puts "Seeding of Event nb #{x}"
 end
 
-puts 'Event created !'
